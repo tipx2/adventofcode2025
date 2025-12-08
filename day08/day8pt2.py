301bdbc8948743b9b660f603c7e5a7e851bb2ca5
tipx2 <tipx2gaming@gmail.com>
2025-12-08 18:25:43 +0000

    day8 improved heapq

--- day08/day8pt2.py
+++ day08/day8pt2.py
@@ -1,25 +1,38 @@
-with open("day08/input8.txt") as f:
-  lines = [x.strip() for x in f.readlines()]
+import heapq, time
 
 def distance(coord1, coord2):
   return (coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2 + (coord1[2] - coord2[2]) ** 2
 
-distances = {}
+start = time.perf_counter()
 
+with open("day08/input8.txt") as f:
+  lines = [x.strip() for x in f.readlines()]
+
+distances = []
 boxes = [(int(x), int(y), int(z)) for x,y,z in [w.split(",") for w in lines]]
-
-for b1 in boxes:
-  for b2 in boxes:
-    if b1 == b2 or (b1, b2) in distances or (b2, b1) in distances:
-      continue
-    distances[(b1, b2)] = distance(b1,b2)
+all_boxes = len(boxes)
 
 
+for b1 in range(all_boxes):
+  for b2 in range(b1+1, all_boxes):
+    distances.append((distance(boxes[b1],boxes[b2]), (boxes[b1], boxes[b2])))
+
+heapq.heapify(distances)
+
+count = 0
 circuits = set()
-for (coord1, coord2), distance in sorted(distances.items(), key=lambda x: x[1]):
-  circuits.add(coord1)
-  circuits.add(coord2)
+while distances:
+  distance, (coord1, coord2) = heapq.heappop(distances)
+  
+  if coord1 not in circuits:
+    circuits.add(coord1)
+    count += 1
+  if coord2 not in circuits:
+    circuits.add(coord2)
+    count += 1
 
-  if all(b in circuits for b in boxes):
+  if count == all_boxes:
     print(coord1[0] * coord2[0])
     break
+
+print(time.perf_counter() - start)
