7807ec4c2d7025791a5475ecdd1f4408c53363c6
tipx2 <tipx2gaming@gmail.com>
2025-12-09 13:24:57 +0000

    day9pt2 progress

--- day09/day9pt2.py
+++ day09/day9pt2.py
@@ -1,22 +1,12 @@
-with open("day09/input9.txt") as f:
+with open("day09/example.txt") as f:
   lines = [x.strip() for x in f.readlines()]
 
 red_tiles = [(int(x), int(y)) for x,y in [z.split(",") for z in lines]]
 green_tiles = set()
 
-minx = 999999999
-maxx = 0
-miny = 999999999
-maxy = 0
-
 for i in range(len(red_tiles)):
   target = red_tiles[i+1] if i != len(red_tiles)-1 else red_tiles[0]
   initial = red_tiles[i]
-  
-  minx = min(minx, initial[0])
-  miny = min(miny, initial[1])
-  maxx = max(maxx, initial[0])
-  maxy = max(maxy, initial[1])
   
   delta = None
   
@@ -36,34 +26,51 @@ for i in range(len(red_tiles)):
     times += 1
     new = (initial[0] + (delta[0] * times), initial[1] + (delta[1] * times))
 
+
+def debug_draw():
+  for y in range(15):
+    for x in range(15):
+      if (x,y) in red_tiles:
+        print("#", end="")
+      elif (x,y) in green_tiles:
+        print("X", end="")
+      else:
+        print(".", end="")
+    print()
+
+# debug_draw()
+
 for l in red_tiles:
   green_tiles.add(l)
 
-print(maxy)
-for y in range(miny, maxy+1):
-  if y % 1000 == 0:
-    print("aryehvalue:", y)
-  spout = False
-  for x in range(minx,maxx+1):
-    if (x,y) in green_tiles and not (x-1,y) in green_tiles:
-      spout = not spout
-    if spout:
-      green_tiles.add((x,y))
+def valid(x1,y1,x2,y2):
+  for x in range(min(x1, x2), max(x1, x2) + 1):
+    if (x,y1) in green_tiles and (x+1,y1) not in green_tiles and (x-1,y1) not in green_tiles:
+      return False
+    if (x,y2) in green_tiles and (x+1,y2) not in green_tiles and (x-1,y2) not in green_tiles:
+      return False
 
-def all_green(l1, l2, green_tiles):
-  for y in range(min(l1[1], l2[1]), max(l1[1], l2[1])):
-    for x in range(min(l1[0], l2[0]), max(l1[0], l2[0])):
-      if (x,y) not in green_tiles:
-        return False
+  for y in range(min(y1, y2), max(y1, y2) + 1):
+    if (x1,y) in green_tiles and (x1,y+1) not in green_tiles and (x1,y-1) not in green_tiles:
+      return False
+    if (x2,y) in green_tiles and (x2,y+1) not in green_tiles and (x2,y-1) not in green_tiles:
+      return False
+    
   return True
 
 largest = 0
 
-for l1 in red_tiles:
-  for l2 in red_tiles:
-    if not all_green(l1, l2, green_tiles):
+for i in range(len(red_tiles)):
+  for j in range(i+1, len(red_tiles)):
+    x1,y1 = red_tiles[i]
+    x2,y2 = red_tiles[j]
+
+    if not valid(x1,y1,x2,y2):
       continue
-    area = abs(l1[0] - l2[0] + 1) * abs(l1[1] - l2[1] + 1)
+
+    area = (abs(x2 - x1) + 1) * (abs(y2 - y1) + 1)
     if area > largest:
       largest = area
+      print(x1,y1,x2,y2, area)
+
 print(largest)
