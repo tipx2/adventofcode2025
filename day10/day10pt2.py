f0a7bee8224d0fa79be84b108f762aa5e2627109
tipx2 <tipx2gaming@gmail.com>
2025-12-10 23:31:30 +0000

    day10pt2 30 min solution

--- day10/day10pt2.py
+++ day10/day10pt2.py
@@ -1,40 +1,61 @@
 import re
-import heapq
+import sympy as sym
+from itertools import product
 
 with open("day10/input10.txt") as f:
     lines = [x.strip() for x in f.readlines()]
 
-def add_clicks(joltage, button):
-    return tuple(joltage[i] + (1 if i in button else 0) for i in range(len(joltage)))
-
-def distance(current_joltage, target_joltage):
-    return sum([abs(current_joltage[i]-target_joltage[i]) for i in range(len(current_joltage))])
+alphabet = list(map(chr, range(97, 123)))
 
 total = 0
-for line in lines:
+for i, line in enumerate(lines):
+    print("aryehvalue:",i)
 
     buttons = tuple(tuple(int(z) for z in x.strip("()").split(",")) for x in re.findall("\([,\d]+\)", line))
     target_joltage = tuple(int(x) for x in re.search("\{[,\d]+\}", line).group().strip("{}").split(","))
-
-    initial_joltage = tuple([0] * len(target_joltage))
-    queue = [(0, initial_joltage)]
-
-    heapq.heapify(queue)
-
-    visited = set()
-    while queue:
-        print(queue)
-        clicks, current_joltage = heapq.heappop(queue)
-        if current_joltage == target_joltage:
-            print(clicks)
-            total += clicks
-            break
-        else:
-            for b in buttons:
-                new = add_clicks(current_joltage, b)
-                if any(target_joltage[i] < new[i] for i in range(len(new))):
-                  continue
-                if new not in visited:
-                    visited.add(new)
-                    queue.append((clicks + 1, new))
+    
+    symbols = [[] for _ in range(len(target_joltage))]
+    eqns = []
+    for i, button in enumerate(buttons):
+      symbol = sym.Symbol(alphabet[i])
+      for b in button:
+        symbols[b].append(symbol)
+    
+    for x in range(len(symbols)):
+      eqns.append(sym.Eq(sum(symbols[x]), target_joltage[x]))
+    
+    result = sym.solve(eqns)
+    
+    frees = set()
+    for r in result.values():
+      frees = frees.union(r.free_symbols)
+    
+    frees = list(frees)
+    upper_bounds = []
+    for symbol in frees:
+      minimum = 999999999999
+      for j in buttons[alphabet.index(symbol.name)]:
+        minimum = min(target_joltage[j], minimum)
+      upper_bounds.append(range(minimum+1))
+    
+    min_solution = 999999999999
+    
+    prev = -1
+    for combination in product(*upper_bounds):
+      if combination and combination[0] != prev:
+        print(combination)
+        prev = combination[0]
+      
+      if sum(combination) >= min_solution:
+        continue
+      
+      solved_values = []
+      for r in result.values():
+        solved_values.append(r.subs({frees[i]:combination[i] for i in range(len(frees))}))
+      
+      if all([x >= 0 and round(x) == round(x, 5) for x in solved_values]):
+        min_solution = min(sum(solved_values) + sum(combination), min_solution)
+    
+    total += min_solution
+    
 print(total)
